9dae1a20064d76dfe4e4d157d6b935d6bef15e4e
Oliver Meyn <oliver@mineallmeyn.com>
2015-06-12 12:14:29 +0200

    initial commit

---
+++ src/main/java/org/gbif/oliver/g17/NeedsG17.java
@@ -0,0 +1,11 @@
+package org.gbif.oliver.g17;
+
+import com.google.common.base.Stopwatch;
+
+public class NeedsG17 {
+
+  public void runMe() {
+    Stopwatch stopwatch = Stopwatch.createStarted();
+    System.out.println("Run me on G17 called.");
+  }
+}
